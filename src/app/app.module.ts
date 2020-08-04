32f0d7e98636bd3bf64b834929247d9a1924d0b9
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2020-08-04 17:18:51 -0300

    separando modulo auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {  HttpClientModule} from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 
 import { headerComponent } from './Header/header.component';
-import { AuthComponent } from './auth/auth.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingModule } from './shopping/shopping.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     headerComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
     RecipesModule,
     ShoppingModule,
-    CoreModule
+    CoreModule, 
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
