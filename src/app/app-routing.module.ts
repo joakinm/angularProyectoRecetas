d662913acdd9df28d5b977f93cd9433f5e8c284c
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2020-08-05 18:40:44 -0300

    implementando preload a modulos con lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { ShoppingComponent } from './shopping/shopping.component';
 import { AuthComponent } from './auth/auth.component';
 
@@ -12,7 +12,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 
