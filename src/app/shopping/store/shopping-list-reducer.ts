a4848cf574532d2b8f85bb3cfeee3ab8ec484b14
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-25 18:01:53 +0100

    creando resto de acciones en reducers, pasando texto a ingles en list actions

--- src/app/shopping/store/shopping-list-reducer.ts
+++ src/app/shopping/store/shopping-list-reducer.ts
@@ -1,26 +1,51 @@
 import { Ingredientes } from "src/app/shared/ingredients.model";
-import * as shoppingListAction from './shopping-list.action';
+import * as shoppingListActions from './shopping-list.action';
 
-const initialState = {ingredientes :
-    [new Ingredientes('manzanas',5),
-    new Ingredientes('papa',2), 
-    new Ingredientes('pera',1)]
+const initialState = {ingredients: [
+    new Ingredientes('manzanas', 5),
+    new Ingredientes('papa', 2), 
+    new Ingredientes('pera', 1)]
 };
 
 export function shoppingListReducer (
     state = initialState,
-    action: shoppingListAction.shoppingListType
+    action: shoppingListActions.shoppingListType
 ) {
     switch (action.type) {
-        case shoppingListAction.ADD_INGREDIENTE:
+        case shoppingListActions.ADD_INGREDIENT:
             return {
                 ...state,
-                ingredientes: [...state.ingredientes, action.payload]
+                ingredientes: [...state.ingredients, action.payload]
             }
-        case shoppingListAction.ADD_INGREDIENTES:
+
+        case shoppingListActions.ADD_INGREDIENTS:
             return {
                 ...state,
-                ingredientes: [...state.ingredientes, ...action.payload]
+                ingredientes: [...state.ingredients, ...action.payload]
             }
+
+        case shoppingListActions.UPDATE_INGREDIENTS:
+            const ingredient = state.ingredients[action.payload.index];
+            const updatedIngredient = {
+                ...ingredient,
+                ...action.payload.ingrediente
+            };
+
+            const updatedIngredients = [...state.ingredients];
+            updatedIngredients[action.payload.index] = updatedIngredient;
+            return {
+                ...state,
+                ingredientes: updatedIngredients
+            }
+
+        case shoppingListActions.DELETE_INGREDIENTS:
+            return {
+                ...state,
+                ingredientes: state.ingredients.filter((ing, ingIndex) => {
+                    return ingIndex !== action.payload;
+                })
+            }
+        default:
+            return state;
     }
 }
--- src/app/shopping/store/shopping-list.action.ts
+++ src/app/shopping/store/shopping-list.action.ts
@@ -1,17 +1,30 @@
 import { Action } from '@ngrx/store';
 import { Ingredientes } from "src/app/shared/ingredients.model";
 
-export const ADD_INGREDIENTE = 'ADD_INGREDIENTE';
-export const ADD_INGREDIENTES = 'ADD_INGREDIENTES';
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENTS = 'UPDATE_INGREDIENTS';
+export const DELETE_INGREDIENTS = 'DELETE_INGREDIENTS';
 
-export class addIngrediente implements Action {
-    readonly type = ADD_INGREDIENTE;   
+export class addIngredient implements Action {
+    readonly type = ADD_INGREDIENT;   
     constructor (public payload: Ingredientes) {}
 }
 
-export class addIngredientes implements Action {
-    readonly type = ADD_INGREDIENTES;   
+export class addIngredients implements Action {
+    readonly type = ADD_INGREDIENTS;   
     constructor (public payload: Ingredientes[]) {}
 }
 
-export type shoppingListType = addIngrediente | addIngredientes;
+export class updateIngredients implements Action {
+    readonly type = UPDATE_INGREDIENTS;   
+    constructor (public payload: {index: number; ingrediente: Ingredientes}) {
+    }
+}
+
+export class deleteIngredientes implements Action {
+    readonly type = DELETE_INGREDIENTS;   
+    constructor (public payload: number) {}
+}
+
+export type shoppingListType = addIngredient | addIngredients | updateIngredients | deleteIngredientes;
