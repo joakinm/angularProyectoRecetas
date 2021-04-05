7eb14440a00d177abc381f7ff25da1c020eed15a
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-04-05 09:34:29 +0200

    probando libreria devtools de ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {  HttpClientModule} from '@angular/common/http';
-
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { headerComponent } from './Header/header.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,10 +11,9 @@ import { ShoppingModule } from './shopping/shopping.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
-import { StoreModule } from '@ngrx/store';
 import { appReducer } from './store/app.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { AuthEffects } from './auth/store/auth.effects';
     CoreModule, 
     AuthModule,
     StoreModule.forRoot(appReducer),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production })
   ],
   bootstrap: [AppComponent]
 })
